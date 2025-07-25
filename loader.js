41a678834bdb9c6c84a84d67133223dc7181403c
nico25m <nickmelzi105@gmail.com>
2025-07-25 16:37:35 +0200

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,10 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Dopo 3 secondi, nasconde il loader e mostra il contenuto
   setTimeout(() => {
-    const loader = document.getElementById("loader");
-    const content = document.getElementById("main-content");
-
-    loader.style.display = "none";
-    content.style.display = "block";
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("main-content").style.display = "block";
   }, 3000);
 });
