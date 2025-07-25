f33d1fbed067b7b303fa894f6f4f0dbc92066bd9
nico25m <nickmelzi105@gmail.com>
2025-07-25 17:57:11 +0200

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,9 +1,11 @@
-document.addEventListener("DOMContentLoaded", function () {
-  setTimeout(() => {  
+window.addEventListener('load', function () {
+  // Appena tutto è caricato, attendi 0.5s e poi rimuovi il loader
+  setTimeout(() => {
     const loader = document.getElementById("loader");
     const content = document.getElementById("main-content");
-
-    loader.style.display = "none";
-    content.classList.remove("hidden");
-  }, 2500);
-  
+    if (loader && content) {
+      loader.style.display = "none";
+      content.classList.remove("hidden");
+    }
+  }, 500); // attesa breve per una transizione fluida
+});
