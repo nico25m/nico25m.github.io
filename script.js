c2d9514557b3049137fb3fc6038f0194c0098579
nico25m <nickmelzi105@gmail.com>
2025-07-26 04:10:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -24,3 +24,14 @@ async function fetchMeteo() {
 }
 
 fetchMeteo();
+
+ const map = L.map('map').setView([45.4642, 9.1900], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([45.4642, 9.1900])
+      .addTo(map)
+      .bindPopup('Ciao da Milano!')
+      .openPopup();
